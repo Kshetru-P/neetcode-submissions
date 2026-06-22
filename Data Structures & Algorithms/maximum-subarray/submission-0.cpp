dcdf22acff6e99154ac3639a18424bf0f5029154
Kshetru-P <pk24mab0a22@student.nitw.ac.in>
2026-06-23 00:02:54 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int cs=0,ms=INT_MIN;
+        for(auto x:nums){
+            cs+=x;
+            ms=max(ms,cs);
+            if(cs<0) cs=0;
+        }
+        return ms;
+    }
+};
