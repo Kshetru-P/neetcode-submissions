e748396c10a8b75cd9de99dccaddfdcba81fcd6d
Kshetru-P <pk24mab0a22@student.nitw.ac.in>
2026-06-27 23:22:20 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2) return n;
+        int p1=2,p2=1;
+        for(int i=3;i<=n;i++){
+            int c=p1+p2;
+            p2=p1;
+            p1=c;
+        }
+        return p1;
+    }
+};
